a2319fdb0dc209bc4101eb2fb9260870456a86f5
s2019w33@e-mirim.hs.kr <s2019w33@e-mirim.hs.kr>
2021-03-29 00:35:36 +0900

    register router finish!

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const port = 5000
 const config = require('./config/key')
 
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const { User } = require('./models/User')
+
+// body-parser를 앱 내에서 사용하려면 다음과 같이 작성
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+
 mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
@@ -12,5 +19,18 @@ mongoose.connect(config.mongoURI, {
 
 
 app.get('/', (req, res) => res.send('Hello world!'))
+
+// register router
+app.post('/register', (req, res) => {
+    const user = new User(req.body)
+
+    // db에 user를 저장
+    user.save((err, userInfo) => {
+        if(err) return res.json({ success: false, err })
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`))
