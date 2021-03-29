f686ff6360906ad85aebb32e79683f064cd45828
s2019w33@e-mirim.hs.kr <s2019w33@e-mirim.hs.kr>
2021-03-29 22:14:01 +0900

    password encryption with Bcrypt

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const saltRounds = 10
 
 const userSchema = mongoose.Schema({
     name: {
@@ -24,6 +26,23 @@ const userSchema = mongoose.Schema({
     },
     tokenExp: {
         type: Number
+    }
+})
+
+userSchema.pre('save', function(next) {
+    var user = this;
+    // 비밀번호 변경 시에만 작동하도록
+    if (user.isModified('password')) {
+        // 암호화
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if (err) return next(err)
+
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if (err) return next(err)
+                user.password = hash
+                next()
+            })
+        })
     }
 })
 
